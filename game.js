3acef0b9e798c215cc57e8ef7d5ef2a09e24a5af
Abigail Chávez <chavezlozanoabigail@gmail.com>
2023-06-17 23:20:12 -0500

    Cambiamos los for por forEach, otra forma de renderizar el mapa

--- game.js
+++ game.js
@@ -25,16 +25,16 @@ function startGame () {
     contextoCanvas.font = elementsSize + 'px Verdana' 
     contextoCanvas.textAlign = 'end'
 
-    /*renderizar el mapa. Crear el array bidimensional de filas y columnas. Limpiando los strings con trim y creando cada elemento del array con split */
     const map = maps[1]
     const mapRows = map.trim().split('\n')
     const mapRowCols = mapRows.map(row => row.trim().split(''))
 
-    /*Se recorre el objeto emojis para encontrar al emoji equivalente al caracter que retorne el array mapRowCols, al que se le recorre tanto las filas como las columnas*/
-    for (let row = 1; row <= 10; row++) {
-        for (let col = 1; col <= 10; col++) {
-            /*Por canvas las iteraciones de los ciclos for, comienzan en 1, pero para recorrer los arrays nesecitamos empezar en 0, por eso en filText, se le resta 1 a row y a col. Asi se renderiza el emoji correcto en la posicion corrrecta del canvas*/
-            contextoCanvas.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * row +12 , elementsSize * col - 8);   
-        }
-    }
+     mapRowCols.forEach((row, rowI)=> {
+        row.forEach((col, colI) => {
+            const emoji = emojis[col]
+            const posX = elementsSize * (rowI + 1)
+            const posY = elementsSize * (colI + 1)
+            contextoCanvas.fillText(emoji, posX + 12, posY - 9)
+        })
+     });
 }
